1369e2c9fe6a336ccfb9c454f31b3e6cbb106581
mughilan16 <mughilant@proton.me>
2023-07-08 14:24:24 +0530

    added rate limit

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -2,10 +2,18 @@ import { createTRPCRouter, privateProcedure, publicProcedure } from "@/server/ap
 import { User, clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis/nodejs";
 
 const filterUserForClient = (user: User) => {
   return { id: user.id, username: user.username, profileImageUrl: user.profileImageUrl }
 }
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true
+})
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -40,6 +48,9 @@ export const postsRouter = createTRPCRouter({
 
   create: privateProcedure.input(z.object({ content: z.string().emoji().min(1).max(280) })).mutation(async ({ ctx, input }) => {
     const authorId = ctx.userId;
+
+    const { success } = await ratelimit.limit(authorId)
+    if (!success) throw new TRPCError({code : "TOO_MANY_REQUESTS"})
 
     const post = await ctx.prisma.post.create({
       data: {
