4ddedf8222032d2b56841c6f97a1e8eb9706da92
mughilan16 <mughilant@proton.me>
2023-07-09 09:26:47 +0530

    post and profile routes created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "@/components/loading";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
+import Link from "next/link";
 
 dayjs.extend(relativeTime)
 
@@ -67,7 +68,10 @@ const PostView = (props: PostWithUser) => {
     <Image src={author.profileImageUrl} alt={`@${author.username} profile picture`} className="rounded-full" width={56} height={56} />
     <div className="flex flex-col">
       <div className="flex text-slate-300 font-semibold gap-1">
-        <span>{`@${author.username}`}</span><span className="font-light">{`· ${dayjs(post.createdAt).fromNow()}`}</span>
+        <Link href={`/@${author.username}`}><span>{`@${author.username}`}</span></Link>
+        <Link href={`/post/${post.id}`}>
+        <span className="font-light">{`· ${dayjs(post.createdAt).fromNow()}`}</span>
+        </Link>
       </div>
       <span className="text-2xl">{post.content}</span>
     </div>
